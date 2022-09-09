864c526f435a4160808ab2c11715f7c18ba270dd
Ddluwole <ddluwole@gmail.com>
2022-09-08 17:25:44 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/** 
- * main - Print the string using printf
- * 
- * this program prints "with proper grammar,but the outcome is a piece of art"
- * Return: 0
+/**
+ * main - print the string in the printf function
+ *  this program prints "Programming is like building a multilingual"
+ *  Return: 0
  */
 int main(void)
 {
